1a7305b487bb89237b165c78ef47a61f3a607f26
Michael Enion <caewok_2000@yahoo.com>
2022-02-21 19:29:11 -0800

    Branches

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/rhead/git/rust-intro/branches/target/debug/branches: /Users/rhead/git/rust-intro/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-d0f82773d78c9658.d
@@ -0,0 +1,5 @@
+/Users/rhead/git/rust-intro/branches/target/debug/deps/branches-d0f82773d78c9658: src/main.rs
+
+/Users/rhead/git/rust-intro/branches/target/debug/deps/branches-d0f82773d78c9658.d: src/main.rs
+
+src/main.rs:
