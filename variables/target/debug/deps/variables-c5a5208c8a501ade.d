a3ec4bd6e4e2b6e9d4296d2dfbe0909f86255b46
Michael Enion <caewok_2000@yahoo.com>
2022-02-21 18:13:11 -0800

    Set up variables example

---
+++ variables/target/debug/deps/variables-c5a5208c8a501ade.d
@@ -0,0 +1,5 @@
+/Users/rhead/git/rust-intro/variables/target/debug/deps/variables-c5a5208c8a501ade: src/main.rs
+
+/Users/rhead/git/rust-intro/variables/target/debug/deps/variables-c5a5208c8a501ade.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/rhead/git/rust-intro/variables/target/debug/variables: /Users/rhead/git/rust-intro/variables/src/main.rs
