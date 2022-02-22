128cb42a75fab11c8ac18c46e113dfd5111763ed
Michael Enion <caewok_2000@yahoo.com>
2022-02-21 21:27:55 -0800

    Ownership example

---
+++ ownership/target/debug/deps/ownership-6a685b0fcc3aea10.d
@@ -0,0 +1,5 @@
+/Users/rhead/git/rust-intro/ownership/target/debug/deps/ownership-6a685b0fcc3aea10: src/main.rs
+
+/Users/rhead/git/rust-intro/ownership/target/debug/deps/ownership-6a685b0fcc3aea10.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/rhead/git/rust-intro/ownership/target/debug/ownership: /Users/rhead/git/rust-intro/ownership/src/main.rs
