c9e361d15c9551a88b442614327bba4ceb34bd6e
Michael Enion <caewok_2000@yahoo.com>
2022-02-21 18:52:25 -0800

    Array element indexing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,25 @@
-fn main() {
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup;
+use std::io;
 
-    println!("The value of y is: {}", y);
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
