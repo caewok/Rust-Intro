e1cb8b3817d1985af842dadae29328118aa8c7e0
Michael Enion <caewok_2000@yahoo.com>
2022-02-22 13:30:55 -0800

    Using impl

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,12 @@ struct Rectangle {
 	height: u32,
 }
 
+impl Rectangle {
+	fn area(&self) -> u32 {
+		self.width * self.height
+	}
+}
+
 fn main() {
     let rect1 = Rectangle {
     	width: 30,
@@ -12,11 +18,7 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
     dbg!(&rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
