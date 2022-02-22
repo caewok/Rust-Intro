764dd2bd76a6b561c071b58a77a33e391a922948
Michael Enion <caewok_2000@yahoo.com>
2022-02-21 18:53:56 -0800

    Functions

---
+++ functions/target/debug/deps/functions-9059252d7df202e6.d
@@ -0,0 +1,5 @@
+/Users/rhead/git/rust-intro/functions/target/debug/deps/functions-9059252d7df202e6: src/main.rs
+
+/Users/rhead/git/rust-intro/functions/target/debug/deps/functions-9059252d7df202e6.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/rhead/git/rust-intro/functions/target/debug/functions: /Users/rhead/git/rust-intro/functions/src/main.rs
